6ecbe93dd0bcf80a3372596fe4b5bcf4e440276b
mcmcmcmc1041206 <109641247+mcmcmcmc1041206@users.noreply.github.com>
2024-07-09 13:49:44 +0800

    set basic Application and Entry Point

---
+++ Hazel/src/Application.cpp
@@ -0,0 +1,9 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	void Application::run() {
+		while (true) {};
+	}
+
+}
---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Application.h"
+
+//-----------Entry Point-------
+#include "EntryPoint.h"
